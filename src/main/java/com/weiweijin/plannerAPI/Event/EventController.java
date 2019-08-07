00d79af78fe9a21bdd6a8356d6f88273441f2acd
Weiwei Jin <weiweijin@proton.attlocal.net>
2019-08-07 11:05:13 -0400

    first commit

---
+++ src/main/java/com/weiweijin/plannerAPI/Event/EventController.java
@@ -0,0 +1,14 @@
+package com.weiweijin.plannerAPI.Event;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EventController {
+
+	@RequestMapping(method = RequestMethod.GET, value ="/")
+	public String getEvents() {
+		return "get events";
+	}
+}
---
+++ src/main/java/com/weiweijin/plannerAPI/PlannerApiApplication.java
@@ -0,0 +1,13 @@
+package com.weiweijin.plannerAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlannerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlannerApiApplication.class, args);
+	}
+
+}
